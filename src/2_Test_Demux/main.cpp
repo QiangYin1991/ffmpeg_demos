12801115a5257730f130773ef1a8d2b182545994
qiangyin <qiangyin@sohu-inc.com>
2021-03-23 00:04:34 +0800

    add

--- src/2_Test_Demux/main.cpp
+++ src/2_Test_Demux/main.cpp
@@ -32,6 +32,42 @@ int main()
 	{
 		std::cout << "Demux success" << std::endl;
 	}
+
+	rc = avformat_find_stream_info(ptrFormatContext, NULL);
+	if (rc)
+	{
+		char buf[1024]{ 0 };
+		av_strerror(rc, buf, sizeof(buf));
+		std::cout << "find stream info failed, error code: " << buf << std::endl;
+	}
+	else
+	{
+		int totalMs = ptrFormatContext->duration / (AV_TIME_BASE / 1000);
+		std::cout << "totalMs: " << totalMs << std::endl;
+	}
+	av_dump_format(ptrFormatContext, 0, nullptr, 0);
+
+	// 获取音视频流信息
+	for (int i = 0; i < ptrFormatContext->nb_streams; i++)
+	{
+		AVStream *ptrStream = ptrFormatContext->streams[i];
+		if (ptrStream->codecpar->codec_type == AVMEDIA_TYPE_AUDIO)
+		{
+			std::cout << "第" << i << "个流是音频流" << std::endl;
+		}
+		else if (ptrStream->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
+		{
+			std::cout << "第" << i << "个流是视频流" << std::endl;
+			std::cout << "宽：" << ptrStream->codecpar->width << std::endl;
+			std::cout << "高：" << ptrStream->codecpar->height << std::endl;
+		}
+	}
+
+	if (ptrFormatContext)
+	{
+		// 释放封装上下文，并设置为NULL
+		avformat_close_input(&ptrFormatContext);
+	}
 	getchar();
 	return 0;
 }
